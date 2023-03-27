df63fdbc6670d2a12ebd6d56a35c00621e55beef
Mathieu HERY <mathieu-ext@spin-interactive.com>
2023-03-27 20:00:54 +0200

    test video player

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,15 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
+import ReactPlayer from "react-player";
 
 export default function HomePage(props) {
-  const [ip, setIP] = useState("");
-
-  const getData = async () => {
-    const res = await axios.get("https://geolocation-db.com/json/");
-    setIP(res.data.IPv4);
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
   return (
-    <div>
-      <h1 style={{ color: "red" }}>Votre IP : {ip}</h1>
-    </div>
+    <ReactPlayer
+      url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+      width="100%"
+      height="100%"
+      controls
+      playing
+      muted
+    />
   );
 }
