e2b0ee2ae8f260a18fab5c0a606e0f2504993800
Mathieu HERY <mathieu1.hery@gmail.com>
2023-03-28 11:24:36 +0200

    update video background

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,13 +3,30 @@ import ReactPlayer from "react-player";
 
 export default function HomePage(props) {
   return (
-    <ReactPlayer
-      url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-      width="100%"
-      height="100%"
-      controls
-      playing
-      muted
-    />
+    <div className="full-width home-video-container">
+      <ReactPlayer
+        className="video"
+        url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        width="100%"
+        height="100%"
+        controls={false}
+        playing
+        muted
+        config={{
+          file: {
+            attributes: {
+              style: {
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+              },
+            },
+          },
+        }}
+      />
+      <div className="content">
+        <h1>totot</h1>
+      </div>
+    </div>
   );
 }
